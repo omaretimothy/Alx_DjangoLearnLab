0c679974708e9a9e7b13b63cdca65a101f85ae36
omaretimothy <omaretimothy53@gmail.com>
2025-05-25 19:28:09 +0300

    Added files for calculting area of rectangle, converting hours into seconds and calculating age of a user in the future

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,6 @@
+current_age =int(input("How old are you? "))
+current_year = 2023
+years_remaining_to_2050 = 2050 - current_year
+user_age_2050 = current_age + years_remaining_to_2050
+
+print ("In 2050, you will be ", user_age_2050, " years old" )
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,6 @@
+hours = 2
+seconds_in_an_hour = 3600 # seconds in an hour
+
+seconds = hours * seconds_in_an_hour
+
+print ("2 hour(s) is ", seconds, " seconds.")

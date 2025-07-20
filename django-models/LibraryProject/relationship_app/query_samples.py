941c418e6b91e525cbba1adf5a2c904750875cec
omaretimothy <omaretimothy53@gmail.com>
2025-07-20 12:05:49 +0300

    fixed library name variable in Library.objects.get() queries

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,19 @@
 from .models import Author, Book, Library, Librarian
 
 # 1. Query all books by a specific author
-specific_author = Author.objects.get(name="George Orwell")
+author_name = "George Orwell"
+specific_author = Author.objects.get(name=author_name)
 books_by_author = Book.objects.filter(author=specific_author)
-print("Books by George Orwell:", list(books_by_author))
+print("Books by", author_name, ":", list(books_by_author))
 
 # 2. List all books in a library
-library = Library.objects.get(name="Central Library")
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
-print("Books in Central Library:", list(books_in_library))
+print("Books in", library_name, ":", list(books_in_library))
 
 # 3. Retrieve the librarian for a library
-library = Library.objects.get(name="Central Library")
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
 librarian = Librarian.objects.get(library=library)
-print("Librarian for Central Library:", librarian.name)
+print("Librarian for", library_name, ":", librarian.name)

0ea6a6f4f553fb75d88415407583383f3fcb09e7
omaretimothy <omaretimothy53@gmail.com>
2025-07-20 13:55:08 +0300

    fixed import permissions

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.contrib.auth import authenticate
-from django.contrib.auth import login   # separate import for checker
+from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import permission_required 
 from django.contrib.auth.models import User
 from django.http import HttpResponseForbidden
 from django import forms
